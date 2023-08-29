144b1089b95454a9d1451179d8b973f57f73adb1
0exia099 <0exia099@gmail.com>
2023-08-30 03:06:59 +0900

    로그인
    로그인

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import Login from "./pageDir/Login";
 import HomeBF from "./pageDir/HomeBF";
 import HomeBeforeBF from "./pageDir/HomeBeforeBF";
 import {Routes, Route} from 'react-router-dom';
+import { useState, useEffect } from "react";
 import ApplyGroup from './pageDir/ApplyGroup';
 
 function App() {
+  const [isLogin, setIsLogin] = useState(false);
+  
   return (
     <div>
-      <NavBar></NavBar>
+      <NavBar isLogin={isLogin}></NavBar>
       <Routes>
         <Route path="/" element={<HomeBF/>}></Route>
         <Route path="/beforeBF" element={<HomeBeforeBF/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/login" element={<Login setIsLogin={setIsLogin}/>}></Route>
         <Route path="/applyGroup" element={<ApplyGroup/>}></Route>
       </Routes>
-      <HomeBF />
     </div>
     // <div className="App">
     //   <header className="App-header">
--- src/pageDir/login.js
+++ src/pageDir/login.js
@@ -1,11 +1,65 @@
-import styles from '../cssDir/login.module.css';
-//import axios from "axios";
-import { useState, useEffect } from "react";
+import styles from '../cssDir/login.module.css'
+import axios from "axios"
+import { useState, useEffect } from "react"
+import {useNavigate} from 'react-router-dom'
 
 const Login = (props) =>{
-    let [id, setId] = useState('');
-    let [pw, setPw] = useState('');
-    //const movePage = useNavigate();
+    let [id, setId] = useState('')
+    let [pw, setPw] = useState('')
+    const movePage = useNavigate()
+
+    const changeId = (e) =>{
+        const value = e.target.value
+        setId(value)
+    }
+    const changePw = (e) => {
+        const value = e.target.value
+        setPw(value)
+    }
+    const goHome = () =>{
+        movePage('/');
+        //window.location.reload();
+    }
+    const login = (e) =>{
+        e.preventDefault()
+        props.setIsLogin(true)
+        
+        //goHome()
+    }
+    // const login = (e) => {
+    //     e.preventDefault()
+    //     setId()
+    //     if (id === '') {
+    //         alert('아이디를 입력해 주세요.')
+    //         return false
+    //     }
+    //     if (pw === '') {
+    //         alert('비밀번호를 입력해 주세요.')
+    //         return false
+    //     }
+    //     let formData = new FormData()
+    //     formData.append("id", id)
+    //     formData.append("password", pw)
+    //     axios.post('/login', formData, {
+    //         headers: {
+    //             "Content-Type": "multipart/form-data",
+    //         },
+    //     })
+    //     .then((response) => {
+    //         if (response.status === 200) {
+    //             console.log('로그인 성공')
+    //             props.setIsLogin("data from child")
+    //             goHome()
+    //         } else {
+    //             console.log('로그인 실패')
+    //         }
+    //     })
+    //     .catch(error => {
+    //         console.error(error)
+    //         console.log('로그인 실패')
+    //         alert('로그인 실패\n이메일과 비밀번호를 확인해 주세요.')
+    //     });
+    // }
 
     return(
         <div className={styles.page}>
@@ -16,15 +70,15 @@ const Login = (props) =>{
                         <div className={styles.loginInputForm}>
                             <form name='login_form' id='login_form' method='post'>
                                 <div className={styles.loginSector}>
-                                    <input type='text' className={styles.loginInput} name='id'></input>
+                                    <input type='text' className={styles.loginInput} name='id' onChange={changeId}></input>
                                     <span className={styles.loginPlaceholder}>ID</span>
                                 </div>
                                 <div className={styles.loginSector}>
-                                    <input type='password' className={styles.loginInput} name='password'></input>
+                                    <input type='password' className={styles.loginInput} name='password' onChange={changePw}></input>
                                     <span className={styles.loginPlaceholder}>Password</span>
                                 </div>
                                 <div className={styles.loginBtnWrap}>
-                                    <button className={styles.loginBtn}>SignIn</button>
+                                    <button className={styles.loginBtn} onClick={login}>SignIn</button>
                                     
                                 </div>
                                 
@@ -39,4 +93,4 @@ const Login = (props) =>{
         </div>
     )
 }
-export default Login;
+export default Login
