46d8b55e060d571a39d295c4aacabb5946714232
0exia099 <0exia099@gmail.com>
2023-08-30 07:40:49 +0900

    대기열 버튼
    대기열버튼

--- src/pageDir/homeBeforeBF.js
+++ src/pageDir/homeBeforeBF.js
@@ -2,11 +2,49 @@ import { useState, useEffect } from "react";
 import ItemCard from "../components/ItemCard";
 import SmallItemCard from "../components/SmallItemCard";
 import style from "../cssDir/homeBeforeBF.module.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 const HomeBeforeBF = (props) => {
+  const movePage = useNavigate();
+  const goLogin = () =>{
+    movePage("/login");
+  }
+  const tryApply = () => {
+    const userId = sessionStorage.getItem("id");
+    if(userId === null){
+      alert("로그인이 필요합니다.")
+      goLogin()
+      return
+    }
+    const data = {
+      'username' : userId,
+    };
+    const config = {
+      "Content-Type": 'application/json',
+      "Access-Control-Allow-Origin": 'http://localhost:3000/',
+      "Access-Control-Allow-Credentials":"true",
+    };
+    axios.post('http://munis.ddns.net:8080/waiting/', data, config)
+    .then((response) => {
+      console.log(response)
+        // if (response.status === 200) {
+        //     console.log('로그인 성공')
+        //     sessionStorage.setItem("id", id);
+        //     props.setIsLogin(true)
+        //     goHome()
+        // } else {
+        //     console.log('로그인 실패')
+        // }
+    })
+    .catch(error => {
+        console.error(error)
+    });
+  }
   return (
     <>
       <div className={style.entireContainer}>
-        <button className={style.bfApplyBtn}>블랙프라이데이 구매 날짜 신청하러 가기</button>
+        <button className={style.bfApplyBtn} onClick={tryApply}>블랙프라이데이 구매 날짜 신청하러 가기</button>
         <div>
           <h1>오늘의 상품</h1>
         </div>
