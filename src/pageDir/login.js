f8ad0dba6654bc270d31b27d4cda95a8d441e960
0exia099 <0exia099@gmail.com>
2023-08-29 22:41:30 +0900

    네브바, 로그인페이지
    네브바, 로그인페이지

---
+++ src/pageDir/login.js
@@ -0,0 +1,35 @@
+import styles from '../cssDir/login.module.css';
+const Login = () =>{
+    return(
+        <div className={styles.page}>
+            <div className={styles.loginPage}>
+                <div className={styles.loginWrap}>
+                    <div className={styles.loginBox}>
+                        <h1 className={styles.loginHeader}>LogIn</h1>
+                        <div className={styles.loginInputForm}>
+                            <form name='login_form' id='login_form' method='post'>
+                                <div className={styles.loginSector}>
+                                    <input type='text' className={styles.loginInput} name='id'></input>
+                                    <span className={styles.loginPlaceholder}>ID</span>
+                                </div>
+                                <div className={styles.loginSector}>
+                                    <input type='password' className={styles.loginInput} name='password'></input>
+                                    <span className={styles.loginPlaceholder}>Password</span>
+                                </div>
+                                <div className={styles.loginBtnWrap}>
+                                    <button className={styles.loginBtn}>SignIn</button>
+                                    
+                                </div>
+                                
+                            </form>
+                            <div className={styles.loginBtnWrap}>
+                                    <button className={styles.signUpBtn}>SignUp</button>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Login;
---
+++ src/pageDir/navBar.js
@@ -0,0 +1,81 @@
+//import {useNavigate} from 'react-router-dom'
+import styles from '../cssDir/navBar.module.css';
+import noMessage from '../imgs/noMessage.png';
+import noMessageHover from '../imgs/noMessage_hover.png';
+import message from '../imgs/message.png';
+import React, {useState} from 'react';
+
+const NavBar = () => {
+    const [isLogin, setIsLogin] = useState(true);
+    // const movePage = useNavigate()
+
+    // const goHome = () => {
+    //     movePage('/')
+    // }
+    return(
+        <div className={styles.navBar}>
+            <div className={styles.logoDiv}>
+                <h1 className={styles.logo}>logo</h1>
+            </div>
+            <div className={styles.leftDiv}>
+                <ul className={styles.navList}>
+                    <li className={styles.navListItem}>
+                        <a className={styles.navListItemText}>
+                            <form name='search' id='search_form' method='get'>
+                                <input type='text' minLength="1" id='searchInput' className={styles.searchInput}
+                                    placeholder='검색' name='search'></input>
+                            </form>
+                        </a>
+                        <button type='submit' form='search_form' id='searchBtn'
+                            className={styles.searchInputBtn}></button>
+                    </li>
+                </ul>
+            </div>
+            
+            <div>
+                
+            </div>
+            <div className={styles.rightDiv}>
+                <ul className={styles.navList}>
+                    {isLogin ? <li className={styles.navListItem}>
+                        <a className={styles.navListItemText}>
+                            <div className={styles.imgBox}>
+                                <img src={noMessage} width={80} height={80} className={styles.noMessageImg}></img>
+                                <img src={noMessageHover} width={80} height={80} className={styles.noMessageHoverImg}></img>
+                            </div>
+                        </a>
+                    </li> : <></>}
+                    {/* <li className={styles.navListItem}>
+                        <a className={styles.navListItemText}>
+                            <div className={styles.imgBox}>
+                                <img src={noMessage} width={80} height={80} className={styles.noMessageImg}></img>
+                                <img src={noMessageHover} width={80} height={80} className={styles.noMessageHoverImg}></img>
+                            </div>
+                        </a>
+                    </li> */}
+                    {isLogin ? <li className={styles.navListItem}>
+                        <a className={styles.navListItemText}>
+                            <span>myPage</span>
+                        </a>
+                    </li>:<></>}
+                    {/* <li className={styles.navListItem}>
+                        <a className={styles.navListItemText}>
+                            <span>myPage</span>
+                        </a>
+                    </li> */}
+                    <li className={styles.navListItem}>
+                        <a className={styles.navListItemText}>
+                            {isLogin ? <span>logout</span> : <span>login</span>}
+                        </a>
+                    </li>
+                    {/* <li className={styles.navListItem}>
+                        <a className={styles.navListItemText}>
+                            <span>logout</span>
+                        </a>
+                    </li> */}
+                </ul>
+            </div>
+        </div>
+    )
+}
+export default NavBar;
